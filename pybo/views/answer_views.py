7ee3ce9299e615da49ae184aa48915bd52aa7f75
Jeon <kuznecov98@naver.com>
2022-08-04 00:07:33 +0900

    3-11

--- pybo/views/answer_views.py
+++ pybo/views/answer_views.py
@@ -4,6 +4,9 @@ from django.http import HttpResponseNotAllowed
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
+from ..forms import AnswerForm
+from ..models import Question, Answer
+
 @login_required(login_url='common:login')
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -46,4 +49,13 @@ def answer_delete(request, answer_id):
         messages.error(request, '삭제권한이 없습니다')
     else:
         answer.delete()
-    return redirect('pybo:detail', question_id= answer.question.id)
+    return redirect('pybo:detail', question_id=answer.question.id)
+
+@login_required(login_url='common:login')
+def answer_vote(request, answer_id):
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user == answer.author:
+        messages.error(request, '본인 글에 추천할 수 없습니다.')
+    else:
+        answer.voter.add(request.user)
+    return redirect('pybo:detail', question_id=answer.question.id)
